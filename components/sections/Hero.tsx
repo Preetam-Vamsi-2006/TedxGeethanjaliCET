f409aaa8a7ccdc7210767dc9f22875ae78164973
Gollapalli Preetam Vamsi <gollapalli.preethamvamshi2006@gmail.com>
2026-08-02 22:23:30 +0530

    Scale blueprint up 25% vertically on mobile only

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -158,10 +158,10 @@ export default function Hero() {
       />
 
       {/* Blueprint - Single SVG with animated clip paths */}
-      <div className="absolute inset-0 flex justify-center items-end z-10 pointer-events-none sm:pt-48 pt-20">
+      <div className="absolute inset-0 flex justify-center items-end z-10 pointer-events-none sm:pt-48 pt-20 sm:scale-100 scale-125">
         <svg
           viewBox="0 0 1167 341"
-          className="sm:w-[95vw] w-full sm:max-w-6xl max-w-none px-2 sm:h-auto h-screen"
+          className="sm:w-[95vw] w-full sm:max-w-6xl max-w-none px-2 sm:h-auto h-96"
           preserveAspectRatio="xMidYMid meet"
           xmlns="http://www.w3.org/2000/svg"
         >
